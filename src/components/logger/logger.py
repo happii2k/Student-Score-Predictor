f567bb1223c2e0d9a85ddb12fcab7062c5b93a6e
happii2k <happywwe2k@gmail.com>
2025-09-07 17:17:02 +0530

    Data ingestion and Transfroming done

--- src/components/logger/logger.py
+++ src/components/logger/logger.py
@@ -2,17 +2,23 @@ import logging
 import os
 from datetime import datetime
 
+# Create logs directory
+LOGS_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOGS_DIR, exist_ok=True)
 
-LOG_FILE  = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+# Log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
 
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    level=logging.INFO
+)
+
+# Create a logger object you can use everywhere
+logger = logging.getLogger("customLogger")
 
 if __name__ == "__main__":
-    logging.info("Logging has started")
+    logger.info("Logging has started")
